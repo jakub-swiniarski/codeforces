85c163be8b0f9a0e18bff18f7905f22485efb64f
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-10-08 21:04:29 +0200

    dragon quest

---
+++ dragon_quest.cpp
@@ -0,0 +1,33 @@
+// https://codeforces.com/problemset/problem/1337/B
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int ile_pytan;
+    cin >> ile_pytan;
+    
+    for (int i = 0; i < ile_pytan; i++) {
+        int hp, n, m;
+        cin >> hp >> n >> m;
+
+        while (n > 0 && hp > (hp / 2) + 10) { // sprawdzamy, czy oplaca sie uzyc tego zaklecia
+            n--;
+            hp = (hp / 2) + 10;
+        }
+
+        hp -= m * 10;
+
+        if (hp <= 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
